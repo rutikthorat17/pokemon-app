4eca7d7d197280435ba350371a2542dfd54d6976
rutikthorat17 <rutikthorat17@gmail.com>
2024-02-19 17:32:49 +0530

    Delivery2
    form to add new entry of pokemon

--- pokemon-list/src/App.js
+++ pokemon-list/src/App.js
@@ -1,20 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PokemonCard from './PokemonCard';
-import './App.css'; // Import CSS for layout
-
-const pokemons = [
-  { name: 'Arcanine', image: '/Arcanine.png' },
-  { name: 'Bulbasaur', image: '/Bulbasaur.png' },
-  { name: 'Charmander', image: '/Charmander.png' },
-  // Add more Pokémon data as needed
-];
+import './App.css'; 
 
 const App = () => {
+  const [pokemons, setPokemons] = useState([
+    { name: 'Pikachu', image: '/pikachu.png' },
+    { name: 'Bulbasaur', image: '/bulbasaur.png' },
+    { name: 'Charmander', image: '/charmander.png' },
+  ]);
+
+  const [newPokemonName, setNewPokemonName] = useState('');
+  const [newPokemonImage, setNewPokemonImage] = useState('');
+
+  const handleNameChange = (e) => {
+    setNewPokemonName(e.target.value);
+  };
+
+  const handleImageChange = (e) => {
+    setNewPokemonImage(e.target.value);
+  };
+
+  const handleAddPokemon = () => {
+    if (newPokemonName && newPokemonImage) {
+      const newPokemon = {
+        name: newPokemonName,
+        image: newPokemonImage,
+      };
+      setPokemons([...pokemons, newPokemon]);
+      setNewPokemonName('');
+      setNewPokemonImage('');
+    }
+  };
+
   return (
     <div className="App">
-      {pokemons.map((pokemon, index) => (
-        <PokemonCard key={index} name={pokemon.name} image={pokemon.image} />
-      ))}
+      <div className="form-container">
+        <input
+          type="text"
+          placeholder="Enter Pokemon Name"
+          value={newPokemonName}
+          onChange={handleNameChange}
+        />
+        <input
+          type="text"
+          placeholder="Enter Pokemon Image URL"
+          value={newPokemonImage}
+          onChange={handleImageChange}
+        />
+        <button onClick={handleAddPokemon}>Add</button>
+      </div>
+      <PokemonCard pokemons={pokemons} />
     </div>
   );
 };
